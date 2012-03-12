9af852335605750a42aff7854421ef470ba611ee
Mike Butsko <butsko.7@osu.edu>
2012-03-12 16:59:09 -0400

    Initial commit

---
+++ page.tpl.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" class="js"><head profile="http://gmpg.org/xfn/11"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+<meta name="description" content="ASC Reports">
+<meta name="author" content="Arts and Sciences Technology Services Web and Data Solutions">
+<meta name="keywords" content="ASC, Reports, Arts and Sciences, OSU, Ohio State">
+
+
+<title>Coming Soon | Arts and Sciences</title>
+
+<link rel="shortcut icon" href="/sites/all/themes/asc_coming_soon/favicon.ico" type="image/x-icon">
+<link type="text/css" rel="stylesheet" media="all" href="/sites/all/themes/asc_coming_soon/css_5338e49f62c42dbe2e99f4e725c2a45c.css">
+<link type="text/css" rel="stylesheet" media="screen, projection" href="/sites/all/themes/asc_coming_soon/css_709fb31c4b84e4bc0a45aae1d2e717df.css">
+<link type="text/css" rel="stylesheet" media="screen, projection, all" href="/sites/all/themes/asc_coming_soon/css_d20ff67e4373d106ff53da616849bf98.css">
+<link type="text/css" rel="stylesheet" media="screen" href="/sites/all/themes/asc_coming_soon/css_014da673c188c8479044b9b331d07bd9.css">
+<link type="text/css" rel="stylesheet" media="print" href="/sites/all/themes/asc_coming_soon/css_ad82afc7cf52b2859e2878cda65b5624.css">
+<link type="text/css" rel="stylesheet" media="all" href="/sites/all/themes/asc_coming_soon/navbar.css">
+<link type="text/css" rel="stylesheet" media="all" href="/sites/all/themes/asc_coming_soon/news.css">
+<link type="text/css" rel="stylesheet" media="all" href="/sites/all/themes/asc_coming_soon/events.css">
+<link type="text/css" rel="stylesheet" media="all" href="/sites/all/themes/asc_coming_soon/people.css">
+<link type="text/css" rel="stylesheet" media="all" href="/sites/all/themes/asc_coming_soon/navbar(1).css">
+<link type="text/css" rel="stylesheet" media="all" href="/sites/all/themes/asc_coming_soon/theme.css">
+<link type="text/css" rel="stylesheet" media="screen, projection" href="/sites/all/themes/asc_coming_soon/screen.css">
+<link type="text/css" rel="stylesheet" media="screen, projection, all" href="/sites/all/themes/asc_coming_soon/main.css">
+<link type="text/css" rel="stylesheet" media="screen" href="/sites/all/themes/asc_coming_soon/header.css">
+<link type="text/css" rel="stylesheet" media="screen" href="/sites/all/themes/asc_coming_soon/footer.css">
+<link type="text/css" rel="stylesheet" media="screen" href="/sites/all/themes/asc_coming_soon/sidebar.css">
+<link type="text/css" rel="stylesheet" media="screen" href="/sites/all/themes/asc_coming_soon/inner.css">
+<link type="text/css" rel="stylesheet" media="screen" href="/sites/all/themes/asc_coming_soon/front.css">
+<link type="text/css" rel="stylesheet" media="print" href="/sites/all/themes/asc_coming_soon/print.css">
+<link type="text/css" rel="stylesheet" media="screen" href="/sites/all/themes/asc_coming_soon/footer(1).css">
+<link type="text/css" rel="stylesheet" media="screen" href="/sites/all/themes/asc_coming_soon/header(1).css">
+<link type="text/css" rel="stylesheet" media="screen" href="/sites/all/themes/asc_coming_soon/theme(1).css">
+
+
+      <style type="text/css">
+      .feature .view-header {
+        background-color:#5c1d26;
+      }
+      .feature .view-content {
+        border-color:#5c1d26;
+      }
+      #front-header-tile {
+        background-color:#371117;
+        background-image:-webkit-gradient(linear, 200 center, 475 center, from(#5c1d26), to(#371117));
+        background-image:-moz-linear-gradient(left,#5c1d26 200px,#371117 475px);
+        -ms-filter:progid:DXImageTransform.Microsoft.gradient(gradientType=1,startColorStr=#5c1d26,endColorStr=#371117);
+        filter:progid:DXImageTransform.Microsoft.gradient(gradientType=1,startColorStr=#5c1d26,endColorStr=#371117);
+      }
+      #front-header > .container {
+        background:#5c1d26;
+      }
+
+    
+        #footer {
+          background-color:#371117;
+          background-image:-webkit-gradient(radial,50% center,0,center center,750,from(#5c1d26),to(#371117));
+          background-image:-moz-radial-gradient(50% center, circle farthest-corner,#5c1d26,#371117);
+          -ms-filter:progid:DXImageTransform.Microsoft.gradient(gradientType=0,startColorStr=#5c1d26,endColorStr=#371117);
+          filter:progid:DXImageTransform.Microsoft.gradient(gradientType=0,startColorStr=#5c1d26,endColorStr=#371117);
+        }
+      </style>
+<script type="text/javascript" src="/sites/all/themes/asc_coming_soon/js_ef70c23eb194a5a5ebf28cc3ed38dc0f.js"></script>
+
+</head>
+
+<body class="not-front not-logged-in page-logout one-sidebar sidebar-right theme-0 layout-c cron-check-processed">
+
+    <div id="osu-Navbar">
+    <p>
+    </p>
+
+    <table id="osu-Navbar-Table"><tbody><tr>
+    <td id="osu-NavbarBreadcrumb-Spacing">&nbsp;
+
+    </td>
+    <td id="osu-Navbar-Content">
+      <div id="osu-NavbarBreadcrumb">
+        <p id="osu">
+          <a title="The Ohio State University homepage" href="http://www.osu.edu/">The Ohio State University</a>
+        </p>
+      </div>
+      <div id="osu-NavbarLinks">
+        <ul>
+          <li><a href="http://www.osu.edu/help.php" title="OSU Help">Help</a></li>
+          <li><a href="http://buckeyelink.osu.edu/" title="Buckeye Link">Buckeye Link</a></li>
+          <li><a href="http://www.osu.edu/map/" title="Campus map">Map</a></li>
+          <li><a href="http://www.osu.edu/findpeople.php" title="Find people at OSU">Find People</a></li>
+          <li><a href="https://webmail.osu.edu/" title="OSU Webmail">Webmail</a></li>
+          <li><a href="http://www.osu.edu/search.php" title="Search Ohio State">Search Ohio State</a></li>
+        </ul>
+      </div>
+    </td>
+    <td>&nbsp;
+
+    </td>
+    </tr></tbody></table>
+  </div>
+  <div id="header">
+    <div class="container">
+      <div id="header-container" class="span-24">
+        <div id="logo" style="background-image: url(/sites/all/themes/asc_coming_soon/header.png); background-repeat: no-repeat;">
+        </div><!-- .logo -->
+        <a href="http://www.osu.edu/" id="osulogo" title="Ohio State University homepage">&nbsp;</a>
+        <div id="navigation">
+
+        </div><!-- #navigation -->
+      </div>
+    </div> <!-- .container -->
+  </div> <!-- #header -->
+  <div id="headerbar">&nbsp;</div>
+
+  		<div id="header-shadow">&nbsp;</div>
+	<div id="main-content" class="container" style="background-image: url('/sites/all/themes/asc_coming_soon/body.png'); height: 477px;">
+	
+			</div><!-- #leftcontent-nostyle -->
+	
+  <hr id="footer-border">
+  <div id="footer">
+    <div class="container">
+              <a href="http://artsandsciences.osu.edu/" id="small-banner-2" title="Arts and Sciences homepage">&nbsp;</a>
+            <div id="contact" class="span-7 append-1">
+        <div class="block block-block" id="block-block-13">
+		<div class="content"><p><strong><a href="mailto:asccomm@osu.edu" title="Visit the contact page.">Contact Us</a></strong><br> <a href="mailto:asccomm@osu.edu" title="Email us">asccomm@osu.edu</a> or 1 614 292-8686</p><p><strong><a href="http://artsandsciences.osu.edu/privacy-policy" title="Privacy Policy">Privacy Policy</a></strong><br> <strong><a href="mailto:asccomm@osu.edu" title=" Contact Us">Questions, Feedback, Accessibility</a></strong> &nbsp;&nbsp;<a href="mailto:asccomm@osu.edu" title="Accessibility"><img alt="Accessibility" height="25" id="footer_accessibility_icon" src="/sites/all/themes/asc_coming_soon/access.png" title="Accessibility" width="25"></a></p></div>
+</div>
+
+      </div><!-- .contact -->
+      <div id="footer-right" class="span-16 last">
+        <div id="footer-top" class="span-16 last">
+          <div class="block block-search" id="block-search-0">
+		<div class="content"><form action="/sites/all/themes/asc_coming_soon/placeholder.html" accept-charset="UTF-8" method="post" id="search-block-form">
+<div><div class="container-inline">
+  <div class="form-item" id="edit-search-block-form-1-wrapper">
+ <label for="edit-search-block-form-1">Search this site: </label>
+</div>
+</div>
+
+</div></form>
+</div>
+</div>
+
+<div class="block block-block" id="block-block-5">
+		<div class="content"><div class="asc-social-media-icons"></div></div>
+</div>
+
+        </div>
+        <div id="footer-bottom" class="span-16 last">
+                  </div>
+      </div>
+    </div><!-- .container -->
+  </div><!-- #footer -->
+
+  <div id="footer_copyright" class="clearfix">
+    <div class="small">
+      <p>© 2011, The Ohio State University, College of Arts and Sciences</p>
+      <p>
+
+<script type="text/javascript">
+  var oldonload = window.onload;
+  var newonload = function(){
+    if(document.getElementById("edit-name")){
+      document.getElementById("edit-name").focus();
+    }
+  };
+  if(typeof window.onload != 'function'){
+    window.onload = newonload;
+  }else{
+    window.onload = function(){
+      oldonload();
+      newonload();
+    };
+  }
+</script>    </div>
+  </div>
+
+
+
+
+
+
+
+</body></html>
